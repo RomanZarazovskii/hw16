3c2e7b270a908e19a59d11b4e3fbea5aece4c612
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2024-10-22 20:20:59 +0300

    update

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,10 @@
+export default function fetchCountries(searchQuery) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${searchQuery}?fields=name,capital,population,languages,flags`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
