71130da4b2bb78dd63704f89270e29de3ac4896f
RomanZarazovskii <124078828+RomanZarazovskii@users.noreply.github.com>
2024-10-29 20:32:14 +0200

    update

--- src/index.js
+++ src/index.js
@@ -7,11 +7,45 @@ const input = document.getElementById('input');
 const list = document.getElementById('list');
 const country = document.getElementById('country');
 
-input.addEventListener('input', debounce(onInputChange, 300));
+input.addEventListener('input', debounce(onInputChange, 500));
 
 function onInputChange(e) {
   e.preventDefault();
 
   const inputValue = e.target.value.trim();
-  console.log(inputValue);
+  if (inputValue === '') {
+    clearData();
+    return;
+  }
+  fetchCountries(inputValue)
+    .then(fetchResponse)
+    .catch(error => {
+      notifyError(`Something wrong ${error}`);
+    });
+}
+
+function clearData() {
+  list.innerHTML = '';
+  country.innerHTML = '';
+}
+
+function renderCountry(countryInfo) {
+  country.insertAdjacentHTML('beforeend', countryInfo);
+}
+
+function renderCountryList(countryList) {
+  country.insertAdjacentHTML('beforeend', countryList);
+}
+
+function fetchResponse(countries) {
+  if (countries.length >= 1 && countries.length <= 10) {
+    const countryList = countries
+      .map(country => {
+        return `
+      <li><img class="list__img" src="${country.flags.svg}"><p>${country.name.official}</p></li>
+      `;
+      })
+      .join('');
+    renderCountryList(countryList);
+  }
 }
